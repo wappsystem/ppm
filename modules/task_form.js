d558b185be1749ee4f22798c7648c9a740e62c1e
Gunnar Unger <gunnar.unger@gmail.com>
2018-08-16 10:04:58 +1000

    update

--- modules/task_form.js
+++ modules/task_form.js
@@ -14,7 +14,6 @@ $vm.autocomplete=function($div,sql,callback){
                 callback(field+'_uid',ui.item.value2,ui.item.value3);
                 for(key in ui.item){
                     if(key.indexOf('field_')!==-1){
-                        alert(11)
                         var k=key.replace('field_','')
                         var v=ui.item[key];
                         callback(k,v);
@@ -25,3 +24,32 @@ $vm.autocomplete=function($div,sql,callback){
     })
 }
 //--------------------------------------------------------
+$vm.autocomplete_split=function($div,sql,callback){
+    var field=$div.attr('data-id');
+    $div.focus(function(){$div.autocomplete("search","");});
+    return $div.autocomplete({
+        minLength:0,
+        source:function(request,response){
+            $VmAPI.request({data:{cmd:'auto',s1:request.term,sql:sql,minLength:0},callback:function(res){
+                for (i=0;i<res.table.length;i++){
+                    res.table[i].name=res.table[i].name.split(';')[0]
+                    res.table[i].value=res.table[i].value.split(';')[1]
+                }
+                response($vm.autocomplete_list(res.table));
+            }});
+        },
+        select: function(event,ui){
+            if(callback!=undefined){
+                //alert(field+'_uid')
+                callback(field+'_uid',ui.item.value2);
+                for(key in ui.item){
+                    if(key.indexOf('field_')!==-1){
+                        var k=key.replace('field_','')
+                        var v=ui.item[key];
+                        callback(k,v);
+                    }
+                }
+            }
+        }
+    })
+}
